75dd07416c76e3ce36b4c764d558d91127ffa401
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-14 23:38:58 +0530

    feat: add request logging middleware and integrate it into the app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+
+const { logger } = require('./middlewares/logger');
 
 // DNS
 const dns = require("dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+app.use(logger);
 
 // health
 app.get("/api/v1/health", (req, res) => {
