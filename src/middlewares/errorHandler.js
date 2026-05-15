4e3690317a5dd4d04aab50737f5bb38a8deae5b3
mohankumar-markuli <mohankumarmarkuli@gmail.com>
2026-05-16 01:55:20 +0530

    refactor: remove console log from error handler middleware

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,5 +1,4 @@
 const errorHandler = (err, req, res, next) => {
-    console.log(err);
     res.status(err.statusCode || err.status || 500).json({
         message: err.message || "Internal Server Error",
     });
